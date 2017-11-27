751fdabfb9b53fd0a18a2205836ce6b81f95dc47
Richard Chan <richardchanme@gmail.com>
2017-11-26 19:52:12 -0800

    Added partial form, routes, controllers and views for editing, updating, and showing form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :posts
   get '/pages', to: 'pages#index'
+  get '/pages/new', to: 'pages#new', as: 'new_page'
+  post '/pages', to: 'pages#create'
   get '/pages/:id', to: 'pages#show', as: 'page'
+  get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
+  patch '/pages/:id', to: 'pages#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
