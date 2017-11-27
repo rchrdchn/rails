e7275bb6b165e7f80e6cbd6aac5928a2a9ae020c
Richard Chan <richardchanme@gmail.com>
2017-11-26 20:22:58 -0800

    Added Delete for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   resources :posts
-  get '/pages', to: 'pages#index'
-  get '/pages/new', to: 'pages#new', as: 'new_page'
-  post '/pages', to: 'pages#create'
-  get '/pages/:id', to: 'pages#show', as: 'page'
-  get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
-  patch '/pages/:id', to: 'pages#update'
+  resources :pages
+  # get '/pages', to: 'pages#index'
+  # get '/pages/new', to: 'pages#new', as: 'new_page'
+  # post '/pages', to: 'pages#create'
+  # get '/pages/:id', to: 'pages#show', as: 'page'
+  # get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
+  # patch '/pages/:id', to: 'pages#update'
+  # delete '/pages/:id', to: 'pages#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
